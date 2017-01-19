bbcb7acfbdc02928883e65c30646c5251bb87245
Mithun Tantri <mithun2693@gmail.com>
2017-01-19 19:16:34 +0530

    Initial Commit

--- api/main.go
+++ api/main.go
@@ -1,9 +1,17 @@
 package main
 
+/*
+Package Dependencies
+gin-gonic/gin
+gorilla/websockets
+*/
 import (
   "github.com/gin-gonic/gin"
 )
 
+/*
+ChatRoom Application written in Golang using Gorilla Websockets & Gin
+*/
 func main(){
-  
+
 }
